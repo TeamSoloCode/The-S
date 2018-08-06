0164b5326f7192f8e2fc5ba92fc9771e831a65d7
Trung Hai <hunghaihuy9@gmail.com>
2018-08-06 22:17:41 +0700

    finish màn hình

--- app/src/main/java/com/example/bruce/myapp/View/DiaryCheckPoint/DiaryCheckPointActivity.java
+++ app/src/main/java/com/example/bruce/myapp/View/DiaryCheckPoint/DiaryCheckPointActivity.java
@@ -17,6 +17,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
